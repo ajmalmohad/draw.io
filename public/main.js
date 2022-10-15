c262bb0bf75e009a6ee31900b744d7b6aa2d4139
ajmalmohad <ajmalajmal.2016@gmail.com>
2022-10-16 01:19:35 +0530

    Rooms

--- public/main.js
+++ public/main.js
@@ -1,29 +1,44 @@
 var socket = io();
 
-function getParams(){
+function getParams() {
     let params = new URLSearchParams(document.location.search);
     let username = params.get("username");
     let roomname = params.get("roomname");
-    return {username, roomname}
+    return { username, roomname }
 }
 
 let chatbox = document.getElementById('chat')
 let message = document.getElementById('submit-img')
-message.addEventListener('click', (e)=>{
+message.addEventListener('click', (e) => {
     let textbox = document.getElementById('text')
     console.log(textbox.value);
-    socket.emit('message', textbox.value)
-    textbox.value = ""
-    textbox.focus()
+    if(textbox.value!=""){
+        socket.emit('message', textbox.value)
+        textbox.value = ""
+        textbox.focus()
+    }
 })
+
+
+document.addEventListener("keyup", function (event) {
+    if (event.keyCode === 13) {
+        let textbox = document.getElementById('text')
+        console.log(textbox.value);
+        if(textbox.value!=""){
+            socket.emit('message', textbox.value)
+            textbox.value = ""
+            textbox.focus()
+        }
+    }
+});
 
 socket.emit('join', getParams())
 
-socket.on('connectToRoom',(message)=>{
+socket.on('connectToRoom', (message) => {
     console.log(message);
 })
 
-socket.on('textMessage', (message)=>{
+socket.on('textMessage', (message) => {
     let div = document.createElement('div')
     div.classList.add('message')
     div.innerHTML = `
